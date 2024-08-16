c4498f24eada149564bc1612528f6031b839ab46
Bayhan_Mali <mali.bayhan@olympicind.com>
2024-08-16 11:25:01 -0700

    Initial

--- cloud_ip_receiver.py
+++ cloud_ip_receiver.py
@@ -1,21 +1,36 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-last_ip = None
+IP_FILE = "last_ip.txt"
+
+
+def load_last_ip():
+    try:
+        with open(IP_FILE, "r") as file:
+            ips = file.readlines()
+            return ips[-1].strip() if ips else None
+    except FileNotFoundError:
+        return None
+
+
+def save_last_ip(ip):
+    with open(IP_FILE, "a") as file:
+        file.write(ip + "\n")
 
 
 @app.route('/update-ip', methods=['POST'])
 def update_ip():
-    global last_ip
     data = request.get_json()
     if "ip" in data:
-        last_ip = data["ip"]
-        return jsonify({"message": "IP updated successfully", "ip": last_ip}), 200
+        ip = data["ip"]
+        save_last_ip(ip)
+        return jsonify({"message": "IP updated successfully", "ip": ip}), 200
     return jsonify({"message": "Invalid request"}), 400
 
 
-@app.route('/get-last-ip', methods=['GET'])
-def get_last_ip():
+@app.route('/', methods=['GET'])
+def show_last_ip():
+    last_ip = load_last_ip()
     if last_ip:
         return jsonify({"last_ip": last_ip}), 200
     return jsonify({"message": "No IP received yet"}), 404
